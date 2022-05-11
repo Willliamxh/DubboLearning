d4bb817a2721cd77be7b20e9e23b0c81a70e629d
Williamxh <821602702@qq.com>
2022-05-11 19:34:26 +0800

    第一章节：spring and springMVC整合结束

--- dubbo-service/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -4,7 +4,7 @@ import com.itheima.service.UserService;
 import org.springframework.stereotype.Service;
 
 
-@Service
+@Service//spring框架注入的service 将该类的对象创建出来 放倒springIOC容器中
 public class UserServiceImpl implements UserService {
 
     public String sayHello() {
