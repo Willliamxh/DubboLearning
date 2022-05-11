e32b5dc7352758866e1fe58d56ceb74abb4949b2
Williamxh <821602702@qq.com>
2022-05-11 19:53:55 +0800

    3.引入dubbo的service，将这个类提供的方法 对外发布 将访问的地址 ip 端口 路径 注册到注册中心中

--- dubbo-service/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.itheima.service.impl;
 
 import com.itheima.service.UserService;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
 
-@Service//spring框架注入的service 将该类的对象创建出来 放倒springIOC容器中
+// @Service//spring框架注入的service 将该类的对象创建出来 放倒springIOC容器中
+@Service //导入 dubbo的service
+//将这个类提供的方法 对外发布 将访问的地址 ip 端口 路径 注册到注册中心中
 public class UserServiceImpl implements UserService {
 
     public String sayHello() {
