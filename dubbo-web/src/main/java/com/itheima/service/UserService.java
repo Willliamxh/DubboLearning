8527d4851c109901420d04bd4120d3b0712dcc43
Williamxh <821602702@qq.com>
2022-05-12 11:17:38 +0800

    访问：http://localhost:8000/user/sayHello.do，成功

--- dubbo-web/src/main/java/com/itheima/service/UserService.java
+++ dubbo-web/src/main/java/com/itheima/service/UserService.java
@@ -4,9 +4,6 @@ package com.itheima.service;
  * @Author Willam_xh
  * @Date 2022-05-12 10:59
  */
-public class UserService {
-
-    public String sayHello() {
-        return "hello dubbo~";
-    }
+public interface UserService {
+    public String sayHello();
 }
