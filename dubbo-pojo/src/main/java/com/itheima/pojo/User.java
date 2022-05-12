078b57195a5f6b9dd2f1e64000705afe2c0e7e8e
Williamxh <821602702@qq.com>
2022-05-12 15:02:49 +0800

    第三章：Dubbo序列化 1.创建pojo类

---
+++ dubbo-pojo/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,60 @@
+package com.itheima.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @Author Willam_xh
+ * @Date 2022-05-10 16:13
+ */
+
+/**
+ * 注意！！！
+ * 将来所有的pojo都要实现Serializable接口
+ */
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
